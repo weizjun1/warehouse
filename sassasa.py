7bd83e08dc7a1ab568a005488718c2b4dd6fde94
weizjun1 <363407392@qq.com>
2019-01-12 01:48:28 -0800

    哈

---
+++ sassasa.py
@@ -0,0 +1,11 @@
+class xinxi:
+    def __init__(self,aa):
+        self.aa=aa
+        print(self.aa)
+    def xinxi1(self):
+        print("姓名：XXX\n"
+              "年龄：xxx\n")
+a=xinxi("haha")
+b=a.xinxi1()
+b
+        
